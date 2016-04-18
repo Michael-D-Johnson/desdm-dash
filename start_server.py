7214e9a5843a1c15d8bf0bc53e13f01d550ad59d
mjohns44 <mjohns44@illinois.edu>
2016-04-18 15:59:19 -0500

    updating server

--- start_server.py
+++ start_server.py
@@ -1,5 +1,5 @@
 from app import app
 
 if __name__=="__main__":
-    #app.run(host='141.142.161.37',debug=True)
-    app.run(debug=True)
+    app.run(host='141.142.161.37',debug=True)
+    #app.run(debug=True)
