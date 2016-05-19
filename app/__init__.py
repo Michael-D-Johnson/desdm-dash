9de39172fb0c4d18c7b68277c8f27fbe1b1fb75f
Michael D. Johnson <mdjohnson44@gmail.com>
2016-05-19 16:52:53 -0500

    removing celery again

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask
 import jinja2 
-from celery import Celery
-import redis
 
 app=Flask(__name__)
 app.jinja_loader = jinja2.FileSystemLoader(['/work/devel/mjohns44/git/desdm-dash/app/templates',
                                             '/work/QA/'])
-
 
 
 from app import views
