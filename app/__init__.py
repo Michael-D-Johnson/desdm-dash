68aef56d1cfcf32dd34297ca3d9d55b5a29489e5
Michael D. Johnson <mdjohnson44@gmail.com>
2016-05-24 14:41:42 -0500

    modifying _init_

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,7 @@ from flask import Flask
 import jinja2 
 
 app=Flask(__name__)
-#app.jinja_loader = jinja2.FileSystemLoader(['/work/devel/mjohns44/git/desdm-dash/app/templates',
-#                                            '/work/QA/'])
-
 app.jinja_loader = jinja2.FileSystemLoader(['/work/devel/mjohns44/git/desdm-dash/app/templates',
-                                            '/work/QA/',
-                                            '/Users/mjohns44/GIT_DESDM/desdm-dash-mjohns44/desdm-dash/app/templates'])
+                                            '/work/QA/'])
 
 from app import views
