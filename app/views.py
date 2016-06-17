3b21b8126f916473d29d67662417bddb3ad967a5
mjohns44 <mjohns44@illinois.edu>
2016-06-17 17:29:36 -0500

    adding titles to html pages

--- app/views.py
+++ app/views.py
@@ -14,44 +14,40 @@ def index():
 @app.route('/processing_archive')
 def processing_archive():
     reqnums = sorted(get_data.processing_archive(),reverse=True)
-    return render_template('processing_archive.html',reqnums = reqnums)
+    return render_template('processing_archive.html',reqnums = reqnums,title='Processing Archive')
 
 @app.route('/processing_summary')
 def processing_summary():
     current_dict,rest_dict,columns,updated = get_data.processing_summary('db-desoper','OPS')
-    return render_template('processing_summary.html',columns=columns,current_dict=current_dict,rest_dict=rest_dict,updated=updated)
+    return render_template('processing_summary.html',columns=columns,current_dict=current_dict,rest_dict=rest_dict,updated=updated,title='Processing Summary')
 
 @app.route('/testing_summary')
 def testing_summary():
     current_dict,rest_dict,columns,updated = get_data.processing_summary('db-desoper','TEST')
     tcurrent_dict,trest_dict,tcolumns,tupdated = get_data.processing_summary('db-destest','TEST')
 
-    return render_template('testing_summary.html',columns=columns,current_dict=current_dict,rest_dict=rest_dict,tcurrent_dict=tcurrent_dict,trest_dict=trest_dict,tcolumns=tcolumns,updated=updated)
+    return render_template('testing_summary.html',columns=columns,current_dict=current_dict,rest_dict=rest_dict,tcurrent_dict=tcurrent_dict,trest_dict=trest_dict,tcolumns=tcolumns,updated=updated,title='Testing Summary')
 
 @app.route('/processing_detail/<reqnum>')
 def processing_detail(reqnum):
     reportname = 'reports/{reqnum}/report_{reqnum}.html'.format(reqnum=reqnum)
-    return render_template('processing_detail_include.html',reportname=reportname)
-
-@app.route('/teff')
-def teff():
-    return render_template('teff.html')
+    return render_template('processing_detail_include.html',reportname=reportname,title='Report {reqnum}'.format(reqnum=reqnum))
 
 @app.route('/dts')
 def dts():
-    return render_template('dts_monitor.html')
+    return render_template('dts_monitor.html',title='DTS')
 
 @app.route('/backups')
 def backups():
-    return render_template('back_ups.html')
+    return render_template('back_ups.html',title='Back ups')
 
 @app.route('/supernova_summary')
 def supernova_summary():
-    return render_template('supernova_summary.html')
+    return render_template('supernova_summary.html',title='Supernova Summary')
 
 @app.route('/coadd_map')
 def coadd_map():
     all_df, processed_df = get_data.create_coadd_map('db-destest',"ME_TEST")
     p = plotter.plot_coadd(all_df, processed_df, "ME_TEST")
     script,div = components(p)
-    return render_template('coadd_map.html', scripts=script, div=div)
+    return render_template('coadd_map.html', scripts=script, div=div,title = 'Coadd Map')
