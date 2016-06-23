aab753e3da09b1386a7945645c09fff9bb4d040f
Albert Bode <abode@deslogin.cosmology.illinois.edu>
2016-06-23 15:27:06 -0500

    Tempory commit of desdf graph to test_df subheader

--- app/views.py
+++ app/views.py
@@ -37,6 +37,13 @@ def processing_detail(reqnum):
 def dts():
     return render_template('dts_monitor.html',title='DTS')
 
+@app.route('/test_df')
+def backups():
+    df = get_data.create_des_usage('db-destest')
+    p = plotter.data_usage_plot(df)
+    script,div = components(p)
+    return render_template('back_ups.html', scripts=script, div=div, title='Backups')
+
 @app.route('/backups')
 def backups():
     df = get_data.create_des_usage('db-destest')
