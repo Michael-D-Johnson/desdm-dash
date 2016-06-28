3d1e68da29d3888a3cc2155b92b46d6106c89695
Albert Bode <abode@deslogin.cosmology.illinois.edu>
2016-06-28 09:26:24 -0500

    fixed import statement

--- app/plotter.py
+++ app/plotter.py
@@ -8,7 +8,6 @@ from bokeh.models.widgets import Panel, Tabs
 from bokeh.models import (HoverTool, BoxSelectTool, BoxZoomTool,FixedTicker,
                           PanTool, ResetTool,WheelZoomTool, ResizeTool,
                           glyphs,Legend,NumeralTickFormatter)
-from bokeh.models.widgets import Panel
 from bokeh.charts import Scatter,Bar,Histogram,color
 
 def plot_times(dataframe):
