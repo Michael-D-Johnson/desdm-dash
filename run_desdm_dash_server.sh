dc3b5c9e8f541faab3106f3b56765593f01e7f41
Albert Bode <abode@deslogin.cosmology.illinois.edu>
2016-07-22 14:51:09 -0500

    Changed bokeh version

--- run_desdm_dash_server.sh
+++ run_desdm_dash_server.sh
@@ -6,7 +6,7 @@ setup -r .
 setup pandas 0.15.2+1
 setup jinja2 2.6+8
 setup opstoolkit 0.1.0+0
-setup bokeh 0.11.0+0
+setup bokeh 0.11.0+1
 setup flask 0.10.1+0
 setup configargparse 0.10.0+0
 python start_server.py
