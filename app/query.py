bece8990fd96248cf61cdfc9b74e64a5db1e598e
Albert Bode <abode@deslogin.cosmology.illinois.edu>
2016-07-22 11:20:49 -0500

    Added Execute statements

--- app/query.py
+++ app/query.py
@@ -160,8 +160,10 @@ def query_desdf(cur):
 
 def get_system_info(start, cur, cur2):
     query = "select number_transferred,number_not_transferred,size_transferred,size_to_be_transferred,number_deprecated,size_deprecated,pipe_processed,pipe_to_be_processed,raw_processed,raw_to_be_processed,run_time from friedel.backup_monitor where run_time >= TO_DATE('%s', 'YYYY-MM-DD HH24:MI:SS') order by run_time desc" % (start.strftime('%Y-%m-%d %H:%M:%S'))
+    cur2.execute(query)
     df = pandas.DataFrame(cur2.fetchall(), columns = ['number_transferred','number_not_transferred','size_transferred','size_to_be_transferred','number_deprecated','size_deprecated','pipe_processed','pipe_to_be_processed','raw_processed','raw_to_be_processed','run_time'])
 
     query2 = "select transfer_date,(tar_size/(1024*1024*1024)),(tar_size/(transfer_time*1024*1024*1024)) from prod.backup_tape where transfer_date is not null and transfer_date >= TO_DATE('%s', 'YYYY-MM-DD HH24:MI:SS') order by transfer_date desc" % (start.strftime('%Y-%m-%d %H:%M:%S'))
+    cur.execute(query2)
     res = pandas.DataFrame(cur.fetchall(), columns = ['tdate','tsize','tav'])
     return df, res
