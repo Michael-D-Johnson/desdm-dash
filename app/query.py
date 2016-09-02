14bd967cf8cd35d5d1965b92d4dcb958d0ae7a4a
Michael D. Johnson <mdjohnson44@gmail.com>
2016-09-02 16:25:02 -0500

    fixing typo in assess query

--- app/query.py
+++ app/query.py
@@ -86,7 +86,7 @@ def assess_query(cur,df,index,triplet,pfw_attempt_id,pipeline):
         assess_q = "SELECT distinct accepted,t_eff,b_eff,c_eff,f_eff,program from finalcut_eval \
                     WHERE pfw_attempt_id={pfwid}".format(pfwid=pfw_attempt_id)
     elif pipeline =='firstcut':
-        assess_q = "SELECT distinct accepted,t_eff,b_eff,c_eff,f_effprogram \
+        assess_q = "SELECT distinct accepted,t_eff,b_eff,c_eff,f_eff,program \
                     FROM firstcut_eval where pfw_attempt_id={pfwid}".format(pfwid=pfw_attempt_id)
     else:
         assess_q = "SELECT distinct accepted,t_eff,b_eff,c_eff,f_eff,program \
