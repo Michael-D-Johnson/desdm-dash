d710cd74eacbe63c3ca2b0146602b1cff1d5a478
ycchen <ycchen@descmp2.cosmology.illinois.edu>
2016-11-17 13:32:18 -0600

    add updating function

---
+++ test_dash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This code runs the server
+# Crontab to create data/csv: 0,15 * * * * /work/devel/mjohns44/git/desdm-dash/app/get_data.sh
+source /work/apps/RHEL6/dist/eups/desdm_eups_setup.sh
+
+setup -r /work/devel/ycchen/git/desdm-dash --nolocks
+setup pandas 0.15.2+1 --nolocks
+setup jinja2 2.6+8 --nolocks
+setup opstoolkit 0.1.0+0 --nolocks
+setup bokeh 0.10.0+0 --nolocks
+setup flask 0.10.1+0 --nolocks
+
+/work/devel/ycchen/git/desdm-dash/app/make_reports.py
